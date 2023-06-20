34183f2c1a30de99bb3a7bbd6046379500ed8724
kenbrawn <131151507+kenbrawn@users.noreply.github.com>
2023-06-20 09:42:46 +0200

    Create accueil.php

---
+++ accueil.php
@@ -0,0 +1,9 @@
+<nav>
+  <ul>
+    <li><a href="#">Accueil</a></li>
+    <li><a href="#">Médicaments</a></li>
+    <li><a href="#">Pharmacies</a></li>
+    <li><a href="#">Commandes</a></li>
+    <li><a href="#">A propos</a></li>
+  </ul>
+</nav>
