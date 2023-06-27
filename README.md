3d57f3e1bb6c0bb37e107d03a363b4cf42be5ac6
kenbrawn <131151507+kenbrawn@users.noreply.github.com>
2023-06-27 06:04:24 +0200

    new

--- README.md
+++
@@ -1,3 +0,0 @@
-# gestion_pharmacie
-## applicaton web pour la gestion de pharmacie avec php et mysql
-## langage de balisage html et de style css
--- recherche/recherche.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET['medicament']) && isset($_GET['pharmacie'])) {
-  $medicament = $_GET['medicament'];
-  $pharmacie = $_GET['pharmacie'];
-  
-  // Traitement de la recherche sur notre page
-  echo 'vous avez achetez cette medicament ';
-}
-?>
