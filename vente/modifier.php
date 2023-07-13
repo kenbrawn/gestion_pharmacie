26f19377eaadd53349184cf4fad8ce70bf11ae3f
kenbrawn <131151507+kenbrawn@users.noreply.github.com>
2023-07-13 09:59:25 +0200

    Update modifier.php

--- vente/modifier.php
+++ vente/modifier.php
@@ -51,66 +51,12 @@ if (isset($_GET['code_vente'])) {
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Modifier le fournisseur</title>
-            <link rel="stylesheet" type="text/css" href="style.css">
-            <style>
-                .form-container{
-                        display: flex;
-                        flex-direction: row;
-                        align-items: center;
-                        margin-top: 5px;
-                      /*  background: url("client.jpg");*/
-
-                   }
-                form {
-                        background-color: #8f9b93;
-                        border-radius: 5px;
-                        box-shadow: 0 0 10px rgba(33, 9, 172, 0.3);
-                        padding: 50px;
-                        width: 300px;
-                   }  
-                   input[type="text"],
-                   input[type="number"],
-                   input[type="date"] 
-                  {
-                        width: 100%;
-                        padding: 10px;
-                        border: 1px solid #ccc;
-                        border-radius: 3px;
-                        margin-bottom: 15px;
-                    }
-                    input[type="submit"] {
-                        background-color: #4CAF50;
-                        color: #fff;
-                        border: none;
-                        padding: 10px;
-                        border-radius: 3px;
-                        cursor: pointer;
-                    }
-                    .container {
-                    display: flex;
-                    align-items: right;
-                    }
-
-                   .image-container {
-                        flex:2;
-                        max-width: 30%;
-                        width:50px;
-                        
-                    }
-
-                   .form-container {
-                       flex: 1;
-                       padding-right:50px; /* Espacement entre l'image et le formulaire */
-                       margin: 5px;
-                     }
-            </style>
+            <link rel="stylesheet" type="text/css" href="../style/vente.css">
         </head>
 
         <body>
             <div class="form-container">
-                <div class="image-container">
-                    <img src="vente.jpg"  alt="Image">
-                </div>
+           
                 <form method="post" action="modifier.php?code_vente=<?php echo $code_vente; ?>">
                     <label for="nom_medicament">Nom du medicament:</label>
                     <input type="text" name="nom_medicament" value="<?php echo $row['nom_medicament']; ?>" required><br>
@@ -127,7 +73,7 @@ if (isset($_GET['code_vente'])) {
 
     <?php
     } else {
-        echo "Aucun client trouvé avec l'id: " . $id_fournisseur;
+        echo "Aucun client trouvé avec l'id: " . $id_vente;
     }
 } else {
     echo "La clé 'id' n'existe pas dans la requête GET.";
