b2ad8cb22cf6577f4dd00cc3faaed746d4a3930e
kenbrawn <131151507+kenbrawn@users.noreply.github.com>
2023-07-13 10:00:55 +0200

    Update recherche.php

--- vente/recherche.php
+++ vente/recherche.php
@@ -47,7 +47,7 @@ if (isset($_POST['search_vente'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css" class="css">
+    <link rel="stylesheet" href="../style/vente.css" class="css">
 </head>
 <body>
     
