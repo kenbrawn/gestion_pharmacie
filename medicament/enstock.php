1d22717e834155cd4b455b2a479824b1e462bded
kenbrawn <131151507+kenbrawn@users.noreply.github.com>
2023-07-22 09:24:21 +0200

    Update enstock.php

--- medicament/enstock.php
+++ medicament/enstock.php
@@ -1,17 +1,13 @@
 <?php
 // Connexion à la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pharmacie";
+include("../connection/connection.php");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Échec de la connexion à la base de données : " . $conn->connect_error);
-}
-
+$page = isset($_GET['page']) ? $_GET['page'] : 1; 
+// Récupère le numéro de page à partir de la requête GET, sinon utilise la page 1 par défaut
+$limit = 4; // Nombre d'éléments par page
+$offset = ($page - 1) * $limit; // Calcul de l'offset
 // Récupérer tous les médicaments en stock
-$sql = "SELECT * FROM medicament ORDER BY nom_medicament";
+$sql = "SELECT code,nom_medicament,designation,nom_fournisseur,prix_medicament,quantite_stock,(prix_medicament * quantite_stock) AS total_prix,date_ajout,date_peremption FROM medicament ORDER BY nom_medicament LIMIT $limit OFFSET $offset";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -19,7 +15,7 @@ if ($result->num_rows > 0) {
 
     // Tableau des médicaments
     echo "<table border='1'>";
-    echo "<tr><th>Code</th><th>Nom du médicament</th><th>Désignation</th><th>Nom fournisseur</th><th>Prix du médicament</th><th>Quantité des stocks</th><th>Date d'ajout</th><th>Date de péremption</th></tr>";
+    echo "<tr><th>Code</th><th>Nom du médicament</th><th>Désignation</th><th>Nom fournisseur</th><th>Prix du médicament($)</th><th>Quantité des stocks</th><th>Prix total($)</th><th>Date d'ajout</th><th>Date de péremption</th><th>modifier</th><th>supprimer</th></tr>";
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo "<td>" . $row['code'] . "</td>";
@@ -28,11 +24,24 @@ if ($result->num_rows > 0) {
         echo "<td>" . $row['nom_fournisseur'] . "</td>";
         echo "<td>" . $row['prix_medicament'] . "</td>";
         echo "<td>" . $row['quantite_stock'] . "</td>";
+        echo"<td>"  . $row['total_prix']     . "</td>";
         echo "<td>" . $row['date_ajout'] . "</td>";
         echo "<td>" . $row['date_peremption'] . "</td>";
+        echo "<td><a href='modifier.php?code=".$row['code']."'>Modifier</a></td>";
+        echo "<td><a href='supprimer.php?code=".$row['code']."'>Supprimer</a></td>";
         echo "</tr>";
     }
     echo "</table>";
+    $resultat_total=mysqli_query($conn,"SELECT COUNT(*) AS total FROM medicament");
+    $ligne_total=mysqli_fetch_array($resultat_total);
+    $total_elements=$ligne_total['total'];
+    $total_pages = ceil($total_elements / $limit); // Nombre total de pages arrondi à l'entier supérieur    
+      // Pagination
+      echo '<div class="pagination">';
+      for ($i = 1; $i <= $total_pages; $i++) {
+          echo '<a href="?page=' . $i . '">' . $i . '</a>&nbsp;';
+      }
+      echo '</div>';
 } else {
     echo "Aucun médicament enregistré dans la base de données.";
 }
