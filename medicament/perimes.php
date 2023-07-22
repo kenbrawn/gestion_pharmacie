d46d9c6f0bdb2c0eba1f7dada56dcc73431f87ff
kenbrawn <131151507+kenbrawn@users.noreply.github.com>
2023-07-22 09:27:12 +0200

    Update perimes.php

--- medicament/perimes.php
+++ medicament/perimes.php
@@ -1,18 +1,16 @@
+
 <?php
 // Connexion à la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pharmacie";
+include("../connection/connection.php");
+$page = isset($_GET['page']) ? $_GET['page'] : 1; 
+// Récupère le numéro de page à partir de la requête GET, sinon utilise la page 1 par défaut
+$limit = 4; // Nombre d'éléments par page
+$offset = ($page - 1) * $limit; // Calcul de l'offset
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Échec de la connexion à la base de données : " . $conn->connect_error);
-}
 
 // Vérifier les médicaments périmés
 
-$sql_perimes = "SELECT * FROM medicament WHERE date_peremption <  NOW() ORDER BY date_peremption";
+$sql_perimes = "SELECT * FROM medicament WHERE date_peremption <  NOW() ORDER BY date_peremption LIMIT $limit OFFSET $offset";
 $result_perimes = $conn->query($sql_perimes);
 
 if ($result_perimes->num_rows > 0) {
@@ -20,7 +18,7 @@ if ($result_perimes->num_rows > 0) {
 
     // Tableau des médicaments périmés
     echo "<table border='1'>";
-    echo "<tr><th>Code</th><th>Nom du médicament</th><th>Désignation</th><th>Nom fournisseur</th><th>Prix du médicament</th><th>Quantité des stocks</th><th>Date d'ajout</th><th>Date de péremption</th></tr>";
+    echo "<tr><th>Code</th><th>Nom du médicament</th><th>Désignation</th><th>Nom fournisseur</th><th>Prix du médicament</th><th>Quantité des stocks</th><th>Date d'ajout</th><th>Date de péremption</th><th>modifier</th><th>supprimer</th></tr>";
     while ($row_perimes = $result_perimes->fetch_assoc()) {
         echo "<tr>";
         echo "<td>" . $row_perimes['code'] . "</td>";
@@ -31,9 +29,21 @@ if ($result_perimes->num_rows > 0) {
         echo "<td>" . $row_perimes['quantite_stock'] . "</td>";
         echo "<td>" . $row_perimes['date_ajout'] . "</td>";
         echo "<td>" . $row_perimes['date_peremption'] . "</td>";
+        echo "<td><a href='modifier.php?code=".$row_perimes['code']."'>Modifier</a></td>";
+        echo "<td><a href='supprimer.php?code=".$row_perimes['code']."'>Supprimer</a></td>";
         echo "</tr>";
     }
     echo "</table>";
+    $resultat_total=mysqli_query($conn,"SELECT COUNT(*) AS total FROM medicament WHERE date_peremption <  NOW() ");
+    $ligne_total=mysqli_fetch_array($resultat_total);
+    $total_elements=$ligne_total['total'];
+    $total_pages = ceil($total_elements / $limit); // Nombre total de pages arrondi à l'entier supérieur    
+      // Pagination
+      echo '<div class="pagination">';
+      for ($i = 1; $i <= $total_pages; $i++) {
+          echo '<a href="?page=' . $i . '">' . $i . '</a>&nbsp;';
+      }
+      echo '</div>';
 } else {
     echo "Aucun médicament périmé.";
 }
