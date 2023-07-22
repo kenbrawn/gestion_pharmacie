ca242af18859696e85fcbf86676b1e7f76e7bd2c
kenbrawn <131151507+kenbrawn@users.noreply.github.com>
2023-07-22 08:37:25 +0200

    Update client.php

--- client/client.php
+++ client/client.php
@@ -1,28 +1,17 @@
 <?php
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nom_client = $_POST['nom_client'];
     $adresse_client = $_POST['adresse_client'];
+    $medicament_acheter=$_POST['medicament_acheter'];
     $telephone = $_POST['telephone'];
     
-    // Connexion à la base de données
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "pharmacie";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Échec de la connexion à la base de données : " . $conn->connect_error);
-    }
-    $page = isset($_GET['page']) ? $_GET['page'] : 1; 
-    // Récupère le numéro de page à partir de la requête GET, sinon utilise la page 1 par défaut
-    $limit = 3; // Nombre d'éléments par page
-    $offset = ($page - 1) * $limit; 
+  include("../connection/connection.php");
     // Insertion des informations dans la base de données
-    $sql = "INSERT INTO client(nom_client, adresse_client, telephone)
-            VALUES ('$nom_client', '$adresse_client', '$telephone')";
+    $sql = "INSERT INTO client(nom_client, adresse_client,medicament_acheter, telephone)
+            VALUES ('$nom_client', '$adresse_client','$medicament_acheter', '$telephone')";
    
-   $total_rows = $result->num_rows; // Nombre total d'éléments
+  
     if ($conn->query($sql) === TRUE) {
         echo "Les informations ont été insérées avec succès dans la base de données.";
     } else {
@@ -31,17 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $conn->close();
 }
-
-// Afficher tous les clients enregistrés dans la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pharmacie";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Échec de la connexion à la base de données : " . $conn->connect_error);
-}
+include("../connection/connection.php");
 $page = isset($_GET['page']) ? $_GET['page'] : 1; 
 // Récupère le numéro de page à partir de la requête GET, sinon utilise la page 1 par défaut
 $limit = 3; // Nombre d'éléments par page
@@ -55,19 +34,24 @@ $result = $conn->query($sql);
 $total_rows = $result->num_rows; // Nombre total d'éléments
 if ($result->num_rows > 0) {
     echo'<header>
-    <h2> Tous Listes des clients  </h2>
+    <h2> Tous les listes des clients  </h2>
 </header>';
     echo'<form method="POST" action="recherche.php">
     <input type="text" name="search_client" placeholder="Rechercher de Client">
-    <button type="submit">Rechercher</button>
-</form>';
-    echo "<table border=1>";
-    echo "<tr><th>ID</th><th>Nom du client</th><th>Adresse</th><th>Téléphone</th><th>Modifier</th><th>Supprimer</th></tr>";
+    <button type="submit">Rechercher</button> ';
+  
+    echo'<div><p>Client qui a achetees de Medicaments a </p>
+    <a href="achetereffetsimediates.php">Effets rapide</a>
+   
+    </form></div>';
+    echo "<table border=1>"; 
+    echo "<tr><th>ID</th><th>Nom du client</th><th>Adresse</th><th>nom medicament</th><th>Téléphone</th><th>Modifier</th><th>Supprimer</th></tr>";
     while ($row = $result->fetch_assoc()) { 
         echo "<tr>";
         echo "<td>" . $row['id_client'] . "</td>";
         echo "<td>" . $row['nom_client'] . "</td>";
         echo "<td>" . $row['adresse_client'] . "</td>";
+        echo "<td>" . $row['medicament_acheter'] . "</td>";
         echo "<td>" . $row['telephone'] . "</td>";
         echo "<td><a href='modifier.php?id_client=".$row['id_client']."'>Modifier</a></td>";
         echo "<td><a href='supprimer.php?id_client=".$row['id_client']."'>Supprimer</a></td>";
