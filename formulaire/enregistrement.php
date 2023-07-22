055d702146783996099595bf89cc7e2f2bf04ab4
kenbrawn <131151507+kenbrawn@users.noreply.github.com>
2023-07-22 08:48:33 +0200

    Update enregistrement.php

--- formulaire/enregistrement.php
+++ formulaire/enregistrement.php
@@ -1,32 +1,25 @@
 <?php
-// Vérifier si le formulaire a été soumis
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupérer les valeurs du formulaire
     $nom_utilisateur = $_POST['nom_utilisateur'];
     $mdp_utilisateur = $_POST['mdp_utilisateur'];
     $confirm_Password = $_POST['confirm_password'];
     $type_utilisateur = $_POST["type_utilisateur"];
+    $email = $_POST['email'];
 
     // Vérifier si les mots de passe correspondent
     if ($mdp_utilisateur === $confirm_Password) {
 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "pharmacie";
-
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Vérification de la connexion à la base de données
-        if (!$conn) {
-            die('Erreur de connexion à la base de données: ' . mysqli_connect_error());
-        }
-
+   include("../connection/connection.php");
         session_start();
         $_SESSION['nom_utilisateur'] = $_POST['nom_utilisateur'];
 
         // Échapper les caractères spéciaux pour éviter les injections SQL
         $nom_utilisateur = mysqli_real_escape_string($conn, $nom_utilisateur);
         $mdp_utilisateur = mysqli_real_escape_string($conn, $mdp_utilisateur);
+        $email = mysqli_real_escape_string($conn, $email);
+
+        
 
         // Vérifier si le nom d'utilisateur existe déjà dans la base de données
         $query = "SELECT * FROM utilisateur WHERE nom_utilisateur = '$nom_utilisateur'";
@@ -35,24 +28,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (mysqli_num_rows($result) > 0) {
             // Le nom d'utilisateur existe déjà
             $message = "Le nom d'utilisateur '$nom_utilisateur' existe déjà. Veuillez choisir un autre nom d'utilisateur.";
+            header("refresh:3; url=../index.php");
         } else {
-            // Le nom d'utilisateur n'existe pas, effectuer l'enregistrement
-            $requete = "INSERT INTO utilisateur (nom_utilisateur, mdp_utilisateur, type_utilisateur) VALUES ('$nom_utilisateur', '$mdp_utilisateur', '$type_utilisateur')";
-
-            if (mysqli_query($conn, $requete)) {
-                $message = "L'utilisateur '$nom_utilisateur' a été enregistré avec succès!";
+            // Vérifier si l'e-mail est valide
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $message = "L'e-mail '$email' n'est pas valide. Veuillez entrer une adresse e-mail valide.";
             } else {
-                $message = "Erreur lors de l'enregistrement de l'utilisateur: " . mysqli_error($conn);
+                // Le nom d'utilisateur n'existe pas et l'e-mail est valide, effectuer l'enregistrement
+                $requete = "INSERT INTO utilisateur (nom_utilisateur, mdp_utilisateur, type_utilisateur, email) VALUES ('$nom_utilisateur', '$mdp_utilisateur', '$type_utilisateur', '$email')";
+
+                if (mysqli_query($conn, $requete)) {
+                    $message = "L'utilisateur '$nom_utilisateur' a été enregistré avec succès!";
+                    // Redirection vers la page d'accueil après 3 secondes
+                    header("refresh:3; url=../accueil/accueil.php");
+                } else {
+                    $message = "Erreur lors de l'enregistrement de l'utilisateur: " . mysqli_error($conn);
+                }
             }
         }
 
         // Fermeture de la connexion à la base de données
         mysqli_close($conn);
-
-        if (isset($message)) {
-            // Redirection vers la page de connexion après 5 secondes
-            header("refresh:5; url=../index.php");
-        }
     } else {
         // Les mots de passe ne correspondent pas
         $message = "Les mots de passe ne correspondent pas. Veuillez réessayer.";
@@ -67,10 +63,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
     <h1>Enregistrement d'un nouvel utilisateur</h1>
-    
     <?php if (isset($message)) { ?>
-        <p><?php echo $message; ?></p>
+        <div class="notification">
+            <p><?php echo $message; ?></p>
+        </div>
     <?php } ?>
-
 </body>
 </html>
