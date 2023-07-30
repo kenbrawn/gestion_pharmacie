cfaaa30e97f1e256514894321fbc4f26c5d25bed
kenbrawn <131151507+kenbrawn@users.noreply.github.com>
2023-07-30 10:20:49 +0200

    Update connection.php

--- connection/connection.php
+++ connection/connection.php
@@ -1,12 +1,12 @@
- <?php  
+ <?PHP  
     // Connexion à la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
+$nom du serveur = "hôte local";
+$Nom d'utilisateur = "racine";
+$Mot de passe = "";
 $dbname = "pharmacie";
 $conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Échec de la connexion à la base de données : " . $conn->connect_error);
+si ($conn-&gt;connect_error) {
+    die("Échec de la connexion à la base de données : " . $conn-&gt;connect_error);
 }
 
 ?>
